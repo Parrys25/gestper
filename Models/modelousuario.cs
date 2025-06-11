73f86a4f45a8e8cbfaf7c51fe4c70532b45dc606
Parrys25 <eduardo.parrys@gmail.com>
2025-06-10 21:07:39 -0400

    error del item 1 corregido

--- Models/modelousuario.cs
+++ Models/modelousuario.cs
@@ -1,24 +1,34 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace Gestper.Models;
-
-public class Usuario
+namespace Gestper.Models
 {
-    [Key]
-    public int IdUsuario { get; set; }
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
 
-    public string Nombre { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre no puede superar los 50 caracteres.")]
+        public string Nombre { get; set; }
 
-    public string Apellido { get; set; }
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El apellido no puede superar los 50 caracteres.")]
+        public string Apellido { get; set; }
 
-    public string Correo { get; set; }
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un correo válido.")]
+        public string Correo { get; set; }
 
-    public string Contrasena { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
+        public string Contrasena { get; set; }
 
-    public string Telefono { get; set; }
+        [Required(ErrorMessage = "El teléfono es obligatorio.")]
+        [Phone(ErrorMessage = "Debe ingresar un número de teléfono válido.")]
+        public string Telefono { get; set; }
 
-    public int IdRol { get; set; }  // Este será 3 por defecto al registrar
+        public int IdRol { get; set; }  // Este será 3 por defecto al registrar
 
-    public bool Activo { get; set; }
-    
+        public bool Activo { get; set; }
+    }
 }
